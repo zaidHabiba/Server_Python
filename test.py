d9080a4a437badafb8a2ed3cccf9bce9f2737acc
zaid habiba <zaid-habiba@hotmail.com>
2019-05-30 09:47:21 +0300

    hello

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 
 import os
+#os.system("start /wait cmd C:\\Users\\mms\\Desktop\\Projects\\PythonTR {git add .}")
 os.system("cd C:\\Users\\mms\\Desktop\\Projects\\PythonTR")
 os.system("git add .")
 os.system("git commit -m \"first commit\"")
